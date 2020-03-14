05a2679bc75ce9f665c6d9825c9338182f42f30b
Ashwin Malik <ashwinmalik92@gmail.com>
2020-03-13 23:03:09 -0400

    Accessibility improvements

--- js/custom.js
+++ js/custom.js
@@ -4,11 +4,13 @@ function topScroll() {
     document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
 
+// Set of functions for loader
+// Reference 1: https://stackoverflow.com/questions/9899372/pure-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-when-t
+// Reference 2: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_loader5
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("content").style.display = "block";
 }
-
 function docReady(fn) {
     if (document.readyState === "complete" || document.readyState === "interactive") {
         setTimeout(fn, 1);
@@ -16,7 +18,6 @@ function docReady(fn) {
         document.addEventListener("DOMContentLoaded", fn);
     }
 }
-
 docReady(function () {
     myVar = setTimeout(showPage, 2000);
 });
